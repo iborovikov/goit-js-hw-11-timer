c55b7b54d3cce0b359b4cd844ad1a49fa017673a
iborovikov <ivans.borovikovs@live.com>
2021-04-04 20:41:35 +0300

    Done

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 
 const tableRef = document.querySelector('#timer-1');
-console.log(tableRef)
+const elements = document.querySelectorAll('#timer-1 span')
+const daysRef = elements[0];
+const hoursRef = elements[2];
+const minutesRef = elements[4];
+const secondsRef = elements[6];
+const targetDate = new Date('Apr 15, 2021');
 
+const CountdownTimer = {
+  start() {
+    setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = targetDate - currentTime;
+      const { days, hours, mins, secs } = timeTransfer(deltaTime);
+      updateClock({ days, hours, mins, secs })
+    }, 1000);
+  }
+};
 
+CountdownTimer.start()
 
-
-
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
-});
+function pad(value) {
+  return String(value).padStart(2, '0')
+};
+function timeTransfer(time) {
+  const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  
+  return { days, hours, mins, secs }
+};
+function updateClock({ days, hours, mins, secs }) {
+  daysRef.textContent = `${days}`;
+  hoursRef.textContent = `${hours}`;
+  minutesRef.textContent = `${mins}`;
+  secondsRef.textContent = `${secs}`;
+};
